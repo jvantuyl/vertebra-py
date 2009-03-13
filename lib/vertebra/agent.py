a991975670e3f4dfbc96be9c1f3ba9fa6e2ca79f
Jayson Vantuyl <jvantuyl@engineyard.com>
2009-03-13 04:22:38 -0700

    Massive Agent Refactor, Initial Fibra Use

--- lib/vertebra/agent.py
+++ lib/vertebra/agent.py
@@ -1,74 +1,97 @@
 from __future__ import with_statement
 from threading import Thread,Condition
-from actor import actor as actor_class
-from logging import debug,info
+from job import job
+from logging import debug,info,warn
 from types import ModuleType,StringTypes
+import fibra
+import fibra.handlers.sleep
+import time
+from sys import exit
+
+# HACK HACK HACK
+fibra.handlers.sleep.time_func = time.time
 
 class agent(Thread):
-  def __init__(self,actors=[],initop=None,idle="wait"):
+  def __init__(self,actors=[],idle="wait"):
+    if isinstance(actors,StringTypes): # Catch strings here
+      actors = [actors]
     super(agent,self).__init__(
       target = self.main,
-      kwargs={'actors':actors, 'initop':initop, 'idle':idle}
+      kwargs={'actors':actors, 'idle':idle}
     )
     self.incalls = {}
     self.outcalls = {}
     self.jobs = []
     self.jobexit = Condition()
     self.actors = []
+    self.sched = fibra.schedule()
     debug("agent initialized")
 
-  def main(self,actors,initop,idle):
+  def main(self,actors,idle):
+    sched = self.sched
     info("agent starting")
-    self.run = True
+    sched.install(self.idle())
+    sched.install(self.recv())
+    sched.install(self.xmit())
     info("loading actors")
     self.load_actor('actor_core')
-    self.load_actors(actors)
-    with self.jobexit:
-      if initop:
-        info("running bootstrap operation: %r" % initop)
-        self.start_job(initop)
-      while 1:
-        debug("main loop exit check")
-        if not self.run:
-          break
-        if not self.jobs and idle == "exit":
-          break
-        self.jobexit.wait()
-    if not self.run:
-      debug("triggered exit")
-    elif idle == "exit" and not self.jobs:
-      debug("finished exit")
+    for actor in actors:
+      try:
+        self.load_actor(actor)
+      except :
+        warn("unable to load %s",actor,exc_info=True)
+    info("agent operational")
+    self.sched.run()
     info("agent terminating")
 
+  def idle(self): # Puts the agent to sleep when there's nothing to be done
+    from time import sleep # FIXME: get this fixed in Fibra
+    ready = True
+    while 1:
+      yield 0.0
+      sleep(0.05) # For now, punt by just sleeping a bit
+
+  def recv(self): # Receive Threadlet
+    info("recv: handler started")
+    while 1:
+      yield 1.0
+      debug("recv: processing")
+
+  def xmit(self): # Transmit Threadlet
+    info("xmit: handler started")
+    while 1:
+      yield 1.0
+      debug("xmit: processing")
+
   def load_actor(self,actor):
+    debug("Trying to load actor %s" % actor)
     if isinstance(actor,ModuleType):
-      for obj in dir(actor):
-        obj = getattr(actor,obj,None)
-        if isinstance(obj,actor_class):
-          self.load_actor(obj)
-    elif isinstance(actor,actor_class):
-      info("loaded actor %r" % actor.__class__.__name__)
-      self.actors.append(actor)
+      actor.load(self) # Ask the actor module to load itself into this agent
     elif isinstance(actor,StringTypes):
       self.load_actor(__import__(actor))
     else:
-      raise NotImplementedError
-      
-  def load_actors(self,actors):
-    for actor in actors:
-      self.load_actor(actor)
+      raise NotImplementedError     
 
-  def start_job(self,op):
-    from time import sleep
-    sleep(5)
-    with self.jobexit:
-      self.jobexit.notifyAll()
+  def do_exit(self):
+    exit(1)
+    yield None
+
+  def stop(self):
+    debug("start shutdown")
+    self.sched.install(self.do_exit())
 
 if __name__ == '__main__':
   import logging
 
   logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s %(levelname)s %(message)s')
-  Agent = agent(initop='blah',idle='exit')
+  Agent = agent(actors=['opstub'],idle='exit')
   Agent.start()
-  Agent.join()
+  try:
+    while 1:
+      Agent.join(0.05)
+  except KeyboardInterrupt:
+    Agent.stop()
+  except:
+    Agent.stop()
+    raise
