d98c6639a1c7370f46b6db0d7299245c56462608
Jayson Vantuyl <jvantuyl@engineyard.com>
2009-04-09 05:05:36 -0700

    Added Toggle to Backoff Randomization + Bug Fix

--- lib/vertebra/conn/backoff.py
+++ lib/vertebra/conn/backoff.py
@@ -1,14 +1,20 @@
 """
 Implementation of Connection Backoff
 """
+from logging import debug
 from random import random
 
-def exponential_backoff(wait_min,wait_max,rate):
+def exponential_backoff(wait_0,wait_max,rate,randomize=True):
   current_try = 0
   while 1:
-    backoff_max = (rate ** current_try) * unit_min
-    backoff_max = min(backoff_max,wait_max)
-    backoff = random * backoff_max
+    backoff_max = (rate ** current_try) * wait_0
+    if wait_max is not None:
+      backoff_max = min(backoff_max,wait_max)
+    if randomize:
+      backoff = random() * backoff_max
+    else:
+      backoff = backoff_max
     debug("backing off: actual=%s, max=%s",(backoff,backoff_max))
     yield backoff
+    current_try += 1
 
