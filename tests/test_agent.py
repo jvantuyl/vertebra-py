2def4227250c62554b58dc3f488fe0cad00722a9
Jayson Vantuyl <jvantuyl@engineyard.com>
2009-04-10 13:35:45 -0700

    Added Core Agent Tests
    * Currently just checks to see if loading nonexistent actor fails

---
+++ tests/test_agent.py
@@ -0,0 +1,11 @@
+from vertebra.agent import mock_agent
+from support import raises
+
+class agent_tests:
+  def setUp(self):
+    self.dummy = mock_agent()
+
+  @raises(ImportError)
+  def test_actor_load_failure(self):
+    """agent: load nonexistent actor fails?"""
+    self.dummy.load_actor('this_actor_doesnt_exist')
