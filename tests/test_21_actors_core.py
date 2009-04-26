216e73cbb2d0fa0913f386e93911053a99507265
Jayson Vantuyl <jvantuyl@engineyard.com>
2009-04-25 19:18:34 -0700

    Updated Tests to Reflect Changes to Agent API

--- tests/test_21_actors_core.py
+++ tests/test_21_actors_core.py
@@ -1,6 +1,12 @@
 from mock_agent import mock_agent
+from mock_conn_base import mock as mock_conn
+from vertebra.config import config
 
 def test_00_load_core():
   """actors.core: actor loads"""
+  conf = config()
+  conf.load()
+  conn = mock_conn()
   agent = mock_agent()
+  agent.setup(conf,conn)
   agent.load_actor("actor_core")
--- tests/test_40_agent.py
+++ tests/test_40_agent.py
@@ -1,9 +1,15 @@
+from vertebra.config import config
 from mock_agent import mock_agent
+from mock_conn_base import mock as mock_conn
 from support import raises
 
 class test_00_agent:
   def setUp(self):
+    conf = config()
+    conf.load()
+    conn = mock_conn()
     self.dummy = mock_agent()
+    self.dummy.setup(conf,conn)
 
   @raises(ImportError)
   def test_00_actor_load_failure(self):
